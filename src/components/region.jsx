c26875e0d4c168014943c5455c3dc886fc383f92
geniuska <aigojie2020@gmail.com>
2023-02-09 12:50:59 +0100

    public

--- src/components/region.jsx
+++ src/components/region.jsx
@@ -3,13 +3,13 @@ export default function Region(){
     return(
         <div className="bg-white shadow text-center px-5 w-[200px] mb-3 py-5 font-bold text-sm rounded-md  focus:ring-blue-500 focus:border-blue-500 block  p-2.5 ">
     
-       <select id="countries" className="bg-white">
+<select className="bg-white border-none focus:border-none">
   <option selected>Choose a region</option>
   <option value="US">United States</option>
   <option value="CA">Canada</option>
   <option value="FR">France</option>
   <option value="DE">Germany</option>
 </select>
-        </div>
+ </div>
     )
 }
