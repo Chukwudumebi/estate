16871ebbc5fc0ce921f083ff62d70c5b036b59cf
geniuska <aigojie2020@gmail.com>
2023-02-20 13:11:16 +0100

    search

--- src/data/users.js
+++ src/data/users.js
@@ -3,8 +3,8 @@ export const Users = [
   { id: 2, username: "HDD02886" },
   { id: 3, username: "HTR02886" },
   { id: 4, username: "HDY02886" },
-  { id: 1, username: "HTP02886" },
-  { id: 2, username: "HDD02886" },
-  { id: 3, username: "HTR02886" },
-  { id: 4, username: "HDY02886" },
+  { id: 5, username: "HTPQ2886" },
+  { id: 6, username: "HDDK2886" },
+  { id: 7, username: "HTR02886" },
+  { id: 8, username: "HDY02886" },
 ];
