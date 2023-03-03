d4fc5f15e4ad1fc4803f15c8780e792365d97c91
geniuska <aigojie2020@gmail.com>
2023-03-03 14:43:24 +0100

    hello

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
   return (
     <Provider>
       <div className="h-screen p-4 pt-24 pb-20 md:pb-10 ">
-        <div className="grid h-full grid-rows-auto-1fr gap-6">
+        <div>
           <Router>
             <Nav />
             <Routes>
