c890b4687c43b5f6c95206b3bbb544c1d7338732
geniuska <aigojie2020@gmail.com>
2023-03-06 14:13:48 +0100

    hello

--- src/pages/display.jsx
+++ src/pages/display.jsx
@@ -74,7 +74,7 @@ function Display( ){
           name="select"
           
           
-          id=""
+        
           className="flex aspect-square h-6 cursor-pointer appearance-none items-center justify-center rounded-md border border-slate-700 bg-slate-200 outline-none after:hidden  after:aspect-square after:h-6 after:rounded-md after:font-['Font_Awesome_5_Free'] after:text-xs after:font-bold after:text-white after:content-['\f00c'] checked:border-transparent checked:after:grid checked:after:items-center checked:after:justify-center after:checked:bg-sky-500"
         />
       </div>
@@ -97,13 +97,15 @@ function Display( ){
       </div> */}
       <div className="table-cell max-h-24 border-b border-b-slate-100 px-[2px] pr-2 text-end align-middle md:px-8">
       
-        <button type="button" className=" mt-1 p-[2px] w-[100px] items-end">
-          <Link
+        
+        
+        <button type="button" className=" mt-1 p-[2px] w-full rounded border border-transparent items-end bg-sky-600 cursor-pointer text-white font-bold ">
+          {/* <Link
             to="activity"
             className="w-max cursor-pointer justify-self-end rounded border border-transparent bg-sky-600 px-[2px] py-[2px] text-xs text-white hover:border-sky-500 hover:bg-white hover:text-sky-600"
-            >
+            > */}
             details
-          </Link>
+    
         </button>
        
       </div>
