1d264f90da9d4793e5f62146af39f54a6740cf70
Chukwudumebi Ughonu <macbook@MacBooks-MacBook-Pro.local>
2023-08-14 12:03:51 +0100

    updating creat listing feature

--- src/components/Filters/TypeSale.jsx
+++ src/components/Filters/TypeSale.jsx
@@ -1,31 +1,37 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import Select from './Select';
+import TextField from '../Inputs/TextField';
 
 const saleOptions = [
-  { label: "Total sale", value: "total" },
-  { label: "Partial sale", value: "partial" },
+  { label: 'Total sale', value: 'total' },
+  { label: 'Partial sale', value: 'partial' },
 ];
 
 // Assuming subdivisions can range from 1 to 10
-const subdivisionOptions = Array.from({length: 10}, (_, i) => ({ label: `${i+1}`, value: i+1 }));
+const subdivisionOptions = Array.from({ length: 10 }, (_, i) => ({ label: `${i + 1}`, value: i + 1 }));
 
 const ownershipOptions = [
-  { label: "Owner", value: "owner" },
-  { label: "Manager", value: "manager" },
+  { label: 'Owner', value: 'owner' },
+  { label: 'Manager', value: 'manager' },
 ];
 
 const rentOptions = [
-  { label: "Rent (per month)", value: "month" },
-  { label: "Rent (per year)", value: "year" },
+  { label: 'Rent (per month)', value: 'month' },
+  { label: 'Rent (per year)', value: 'year' },
 ];
 
 const taxesOptions = [
-  { label: "Taxes (per month)", value: "month" },
-  { label: "Taxes (per year)", value: "year" },
+  { label: 'Taxes (per month)', value: 'month' },
+  { label: 'Taxes (per year)', value: 'year' },
+];
+
+const ageOptions = [
+  { label: 'Years', value: 'years' },
+  { label: 'Months', value: 'months' },
 ];
 
 function SaleType() {
-  const [saleType, setSaleType] = useState("total");
+  const [saleType, setSaleType] = useState('total');
   const [subdivisions, setSubdivisions] = useState(subdivisionOptions[0]);
   const [selectValue, setSelectValue] = useState(subdivisionOptions[0]);
   const [ownership, setOwnership] = useState(ownershipOptions[0]);
@@ -34,6 +40,7 @@ function SaleType() {
   const [taxes, setTaxes] = useState(0);
   const [taxesPeriod, setTaxesPeriod] = useState(taxesOptions[0]);
   const [age, setAge] = useState(0);
+  const [agePeriod, setAgePeriod] = useState(rentOptions[0]);
   const [tenants, setTenants] = useState([]);
   const [price, setPrice] = useState(0);
   const [images, setImages] = useState([]);
@@ -45,15 +52,15 @@ function SaleType() {
   const handleSubmit = (e) => {
     e.preventDefault();
     // eslint-disable-next-line no-console
-    console.log("Form submitted");
+    console.log('Form submitted');
   };
-  
+
   const handleAddTenant = () => {
-    console.log("Tenant added");
+    console.log('Tenant added');
   };
 
   const handleAddImage = () => {
-    console.log("Image added");
+    console.log('Image added');
   };
   return (
     <form onSubmit={handleSubmit} className="space-y-4">
@@ -70,9 +77,9 @@ function SaleType() {
           name="saleType"
         />
       </div>
-      {saleType === "partial" && (
+      {saleType === 'partial' && (
         <div className="mb-3">
-          <label className="text-sm font-medium text-gray-700 mb-1" htmlFor="subdivisions">
+          <label className="mb-1 text-sm font-medium text-gray-700" htmlFor="subdivisions">
             Subdivisions
           </label>
           <Select
@@ -89,7 +96,7 @@ function SaleType() {
         </div>
       )}
       <div className="mb-3">
-        <label className="text-sm font-medium text-gray-700 mb-1" htmlFor="ownership">
+        <label className="mb-1 text-sm font-medium text-gray-700" htmlFor="ownership">
           Ownership
         </label>
         <Select
@@ -101,31 +108,32 @@ function SaleType() {
           name="ownership"
         />
       </div>
-      <div className="mb-3">
-  <label className="text-sm font-medium text-gray-700 mb-1" htmlFor="rent">
-    Rent
-  </label>
-  <Select
-    id="rent"
-    options={rentOptions}
-    value={rentPeriod}
-    onChange={(value) => setRentPeriod(value)}
-    placeholder="Select a rent period"
-    name="rent"
-  />
-  <input
-    type="number"
-    id="rentValue"
-    min={0}
-    value={rent}
-    onChange={(e) => setRent(e.target.value)}
-    className="w-full"
-  />
-</div>
+      <div className="mb-3 space-y-1">
+        <label className="mb-1 text-sm font-medium text-gray-700" htmlFor="rent">
+          Rent
+        </label>
+        <Select
+          id="rent"
+          options={rentOptions}
+          value={rentPeriod}
+          onChange={(value) => setRentPeriod(value)}
+          placeholder="Select a rent period"
+          name="rent"
+        />
+        <TextField
+          type="number"
+          id="rentValue"
+          placeholder='Enter rent value'
+          min={0}
+          value={rent}
+          onChange={(e) => setRent(e.target.value)}
+          className="w-full bg-slate-100"
+        />
+      </div>
 
-      <div className="mb-3">
-        <label className="text-sm font-medium text-gray-700 mb-1" htmlFor="taxes">
-          Taxes 
+      <div className="mb-3 space-y-1">
+        <label className="mb-1 text-sm font-medium text-gray-700" htmlFor="taxes">
+          Taxes
         </label>
         <Select
           id="taxes"
@@ -134,28 +142,30 @@ function SaleType() {
           onChange={(value) => setTaxesPeriod(value)}
           placeholder="Select a taxes period"
           name="taxes"
-          />
-        <input
+        />
+        <TextField
           type="number"
           id="taxes"
           min={0}
           value={taxes}
+          placeholder='Enter taxes value'
           onChange={(e) => setTaxes(e.target.value)}
           className="w-full"
         />
       </div>
-      <div>
-        <label className="text-sm font-medium text-gray-700 mb-1" htmlFor="age">
+      <div className='space-y-1'>
+        <label className="mb-1 text-sm font-medium text-gray-700" htmlFor="age">
           Age (in years)
         </label>
-        <input
-          type="number"
-          id="age"
-          min={0}
-          value={age}
-          onChange={(e) => setAge(e.target.value)}
-          className="w-full"
+        <Select
+        id="age"
+        options={ageOptions}
+        value={agePeriod}
+        onChange={(value) => setAgePeriod(value)}
+        placeholder="Select an age period"
+        name="age"
         />
+        <TextField type="number" id="age" min={0} value={age} placeholder='Enter value for age' onChange={(e) => setAge(e.target.value)} className="w-full" />
       </div>
       {/* <div>
         <label className="text-sm font-medium mb-1" htmlFor="tenants">
@@ -181,17 +191,15 @@ function SaleType() {
             <li key={index}>{tenant}</li>
           ))}
         </ul> */}
-        {/* <ul className="mt-2 space-y-1">
+      {/* <ul className="mt-2 space-y-1">
   {tenants.map((tenant) => (
     <li key={tenant}>{tenant}</li>
   ))}
 </ul>
 
-      </div> */} 
+      </div> */}
     </form>
   );
 }
 
 export default SaleType;
-
-
